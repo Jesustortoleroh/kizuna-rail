4d40292b0cfa717b2fbae16a2ea4aba3605f8faf
Jesustortoleroh <tor19021@byui.edu>
2026-05-22 21:21:21 -0400

    Enhance filter functionality and update CSS layout for improved user experience

--- src/routes/routes/list.js
+++ src/routes/routes/list.js
@@ -2,8 +2,19 @@ import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/m
 
 export default async (req, res) => {
     const regions = await getListOfRegions();
-    const routes = await getAllRoutes();
+    // I changed const routes = await getAllRoutes(); to let so the routes could be updated.
+    let routes = await getAllRoutes();
     const seasons = await getListOfSeasons();
+
+    const {region, season} = req.query
+
+    if (region) {
+        routes = routes.filter(route => route.region === region);
+    }
+
+    if (season) {
+        routes = routes.filter(routes => routes.bestSeason === season);
+    }
 
     res.render('routes/list', { 
         title: 'Scenic Train Routes',
